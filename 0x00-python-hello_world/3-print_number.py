42e210cbe189d7f1cc45466f4b38dfb349cacabe
Ayoub <ayoubzerou@gmail.com>
2024-09-09 06:20:48 -0700

    task 3

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,3 +1,55 @@
 #!/usr/bin/python3
 number = 98
-print("{:d} Battery street".format(number))
+print(f"{number} Battery street")
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+-- INSERT --                                                                              3,1           All
+
