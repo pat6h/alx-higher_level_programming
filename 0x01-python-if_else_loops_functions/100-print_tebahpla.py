d049e94a51253c13ff3c3b3bb89f2ae4098df428
Ayoub <ayoubzerou@gmail.com>
2024-09-10 15:14:29 -0700

    task

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,2 +1,5 @@
 #!/usr/bin/python3
-print(''.join([chr(i) if i % 2 == 0 else chr(i - 32) for i in range(122, 96, -1)]), end='')
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
