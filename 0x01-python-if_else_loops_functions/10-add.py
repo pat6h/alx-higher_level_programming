ae2a465f88ff644fcea69523342e35292e2f0553
Ayoub <ayoubzerou@gmail.com>
2024-09-10 14:20:59 -0700

    task

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,9 +1,3 @@
 #!/usr/bin/env python3
 def add(a, b):
-    if b > 0:
-        for _ in range(b):
-            a += 1
-    else:
-        for _ in range(abs(b)):
-            a -= 1
-    return a
+    return (a + b)
