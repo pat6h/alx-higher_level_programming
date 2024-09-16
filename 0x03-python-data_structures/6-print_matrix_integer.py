7328623d96a939afc42abe1bc8b769734351110d
Ayoub <ayoubzerou@gmail.com>
2024-09-16 15:48:35 -0700

    task

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-
-def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-                print("{:d}".format(matrix[i][j]), end="")
-                if j != (len(matrix[i]) - 1):
-                    print(" ", end="")
-
-        print("")
+def max_integer(my_list=[]):
+    if my_list:
+        imax = my_list[0]
+        for i in my_list:
+            if i > imax:
+                imax = i
+        return imax
+    return None
