6fde4e6e017f2fe675e33fe7464eff596efbec61
Ayoub <ayoubzerou@gmail.com>
2024-10-08 11:05:41 -0700

    alot

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(static={"count": 0}):
-    static["count"] += 1
-    return str("Holberton, " * static["count"])[:-2]
+def magic_string(Holby=[]):
+    Holby += ["BestSchool"]
+    return (", ".join(Holby))
