cd7fa6d0961acbfbe3d14434632530783d9c12fb
Ayoub <ayoubzerou@gmail.com>
2024-12-19 16:21:14 -0800

    tasks

--- 0x13-javascript_objects_scopes_closures/102-sorted.js
+++ 0x13-javascript_objects_scopes_closures/102-sorted.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
-let args = process.argv.slice(2);
-if (args.length < 3) {
-  console.log('Usage: ./102-concat.js fileA fileB fileC');
-  process.exit(1);
-}
-let fa = fs.readFileSync(args[0]);
-let fb = fs.readFileSync(args[1]);
-fs.writeFileSync(args[2], fa + fb);
+
+const fArg = fs.readFileSync(process.argv[2]).toString();
+const sArg = fs.readFileSync(process.argv[3]).toString();
+fs.writeFileSync(process.argv[4], fArg + sArg);
--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,23 +1,32 @@
 #!/usr/bin/node
-module.exports = class Rectangle {
+class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) {
+    if ((w > 0) && (h > 0)) {
       this.width = w;
       this.height = h;
     }
   }
+
   print () {
     for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += 'X';
+      }
+      console.log(s);
     }
   }
+
   rotate () {
-    let tmp = this.width;
+    const aux = this.width;
     this.width = this.height;
-    this.height = tmp;
+    this.height = aux;
   }
+
   double () {
     this.width *= 2;
     this.height *= 2;
   }
-};
+}
+
+module.exports = Rectangle;
